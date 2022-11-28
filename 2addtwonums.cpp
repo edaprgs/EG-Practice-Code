0c2dc8c112386465e8bce291006fe294e007df1f
Eda Grace Paragoso <117576162+edaprgs@users.noreply.github.com>
2022-11-28 20:27:48 +0800

    Create 2addtwonums.cpp

---
+++ 2addtwonums.cpp
@@ -0,0 +1,35 @@
+// Write a C++ program to add two numbers
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int intfirst, intsecond;
+    double firstD, secondD, sum;
+
+    cout << "Enter first integer number: ";
+    cin >> intfirst;
+
+    cout << "Enter second integer number: ";
+    cin >> intsecond;
+
+    cout << "The sum of the integer numbers: " << intfirst+intsecond;
+
+    cout << "\n\n";
+    cout << "__________________________________";
+    cout << "\n\n";
+
+    cout << "Enter first decimal number: ";
+    cin >> firstD;
+
+    cout << "Enter second decimal number: ";
+    cin >> secondD;
+
+    sum = firstD + secondD;
+
+    cout << "The sum of the decimal numbers: " << sum;
+    cout << "\n";
+
+    return 0;
+}
