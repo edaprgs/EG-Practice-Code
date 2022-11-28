4fa2fcb9803b2bdd0bdae8e4be57c407dfef85fc
Eda Grace Paragoso <117576162+edaprgs@users.noreply.github.com>
2022-11-28 20:13:16 +0800

    Create displaynumber.cpp

---
+++ displaynumber.cpp
@@ -0,0 +1,21 @@
+// Write a C++ program to display number (entered by the user)
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int intnum;
+    float decnum;
+
+    cout << "Enter an integer number: ";
+    cin >> intnum;
+
+    cout << "Enter a decimal number: ";
+    cin >> decnum;
+
+    cout << "You entered the integer number: " << intnum << endl;
+    cout << "You entered the decimal number: " << decnum << endl;
+
+    return 0;
+}
