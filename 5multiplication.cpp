67a236e4ed5bb03de8a35f4fddf08d8134993aba
Eda Grace Paragoso <117576162+edaprgs@users.noreply.github.com>
2022-11-29 14:46:35 +0800

    Create 5multiplication.cpp

---
+++ 5multiplication.cpp
@@ -0,0 +1,22 @@
+// C++ Multiplication of Two Numbers
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+     int multiplicand, multiplier, product;
+     
+     cout << "Enter the multiplicand: ";
+     cin >> multiplicand;
+     
+     cout << "Enter the multiplier: ";
+     cin >> multiplier;
+     
+     product = multiplicand*multiplier;
+     
+     cout << "The product is: "<<product<<"";
+     
+     return 0;
+}
